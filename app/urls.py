7701daaa5eec67e16171869d2829e13f4909926a
ssshmyaks <153379674+ssshmyaks@users.noreply.github.com>
2024-10-11 01:02:53 +1000

    main

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.main, name='home'),
+    path('index.html', views.main, name='home'),
+    path('portfolio.html', views.portfolio, name='portfolio'),
+    path('contact.html', views.contact, name='contact')
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, 'main.html')
+def main(request):
+    return render(request, 'index.html')
+
+
+def portfolio(request):
+    return render(request, 'portfolio.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
